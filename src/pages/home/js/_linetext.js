1f6c3267110f881353a2e07f48c9a2c25a4275ad
zhangzn3 <619025283@qq.com>
2017-06-23 18:14:16 +0800

    mmmmmm

--- src/pages/home/js/_linetext.js
+++ src/pages/home/js/_linetext.js
@@ -2,24 +2,17 @@
  * Created by Administrator on 2017/6/17.
  */
 export default function(json,vis){
-    return vis.selectAll('.linetext')
-        .data(json.links)
-        .enter()
-        .append("text")
-        .append('svg:textPath')
-        .attr("class","linetext")
-        .attr("startOffset", "50%")
-        .attr("text-anchor", "middle")
-        .attr("xlink:href", function(d) {
-            if (d.source.index == d.target.index) {
-                return false; //不应该有指向自己的关系 异常处理
-            } else {
-                return "#" + d.source.index + "_" + d.target.index;
-            }
-        })
-        .text(function(d){
-            return d.relation;  //关系文字
-        })
-        .attr('fill','#18a1cf');
+     let _linetext=vis.selectAll('.linetext');
+     _linetext=_linetext.data(json.links,(d)=>(`${d.source.name}_${d.target.name}`));
+     _linetext.exit().remove();
+     _linetext=_linetext.enter().append("text").attr("class","linetext").merge(_linetext);
+     _linetext.selectAll().remove().append('svg:textPath').attr("startOffset", "50%")
+     .attr("text-anchor", "middle")
+     .attr("xlink:href", (d)=>{ //不应该有指向自己的关系 异常处理
+     return d.source.index === d.target.index?false:`#${d.source.index}_${d.target.index}`
+     })
+     .text((d)=>(d.relation))//关系文字
+     .attr('fill','#18a1cf');
+     return _linetext;
 }
 
