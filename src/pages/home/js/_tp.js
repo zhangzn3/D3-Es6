3ddaf8498d251bee814fee66692df7de84aa72f9
zhangzn3 <619025283@qq.com>
2017-06-24 13:45:15 +0800

    mmmmmm

--- src/pages/home/js/_tp.js
+++ src/pages/home/js/_tp.js
@@ -7,15 +7,18 @@ export default function(highlightObject,tooltip){
             $('.tooltip').off().on('click', '.cm-btn.collapseCurNode',()=>{ highlightObject(obj);})
         },
         highlightToolTip(obj){
+            let toolTpl=`
+                <div class='title'>编辑</div>
+                <table class='detail-info'>
+                    <tr><td><span class='cm-btn collapseCurNode' >收起当前节点</span></td></tr>
+                    <tr><td>菜单选项二</td></tr><tr><td>菜单选项三</td></tr>
+                </table>
+            `;
             if (obj) {
-                tooltip.html("<div class='title'>编辑</div><table class='detail-info'><tr><td><span class='cm-btn collapseCurNode' >收起当前节点</span></td></tr>" +
-                        "<tr><td>菜单选项二</td></tr><tr><td>菜单选项三</td></tr></table>")
-                    .style("left", (d3.event.pageX + 20) + "px")
-                    .style("top", (d3.event.pageY - 20) + "px")
-                    .style("opacity", 1.0);
+                tooltip.html(toolTpl).style("left",`${d3.event.pageX}px`).style("top",`${d3.event.pageY}px`).style("display","block");
                 this.bindMenuEvent(obj);
             } else {
-                tooltip.style("opacity", 0.0);
+                tooltip.style("display",'none');
             }
         }
     }
