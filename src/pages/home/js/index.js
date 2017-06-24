8bd686775f6fe4c4e7607f381c1e7e09f9dbe93d
zhangzn3 <619025283@qq.com>
2017-06-24 15:35:08 +0800

    d3

--- src/pages/home/js/index.js
+++ src/pages/home/js/index.js
@@ -15,10 +15,10 @@ import _nodeDrag from './_nodeDrag.js'; //引入节点拖拽模块
 import _bindEvent from './_bindEvent.js';//工具栏操作
 import _tick from './_tick.js';//引入更新坐标模块
 let json=require('../data/data1.json');//获取数据
-let vis=_vis();
-let force=_force(json);
-let bindEvent=_bindEvent(json,dialog,update,svg2Png);
-let tooltip=d3.select("body").append("div").attr("class", "tooltip");
+let vis=_vis();//创建svg视图
+let force=_force(json);//力导向图布局
+let bindEvent=_bindEvent(json,dialog,update,svg2Png);//绑定工具栏的操作事件
+let tooltip=d3.select("body").append("div").attr("class", "tooltip");//添加悬浮窗元素
 let dependsNode=[],dependsLinkAndText=[];
 
 //节点显示隐藏
@@ -28,87 +28,57 @@ function highlightObject(obj){
         let objIndex= obj.index;
         dependsNode=dependsNode.concat([objIndex]);
         dependsLinkAndText=dependsLinkAndText.concat([objIndex]);
-        allNode.classed('inactive',function(d){
-            return (dependsNode.indexOf(d.index) > -1)
-        });
-        allLink.classed('inactive', function(d) {
-            return ((dependsLinkAndText.indexOf(d.source.index) > -1) || (dependsLinkAndText.indexOf(d.target.index) > -1))
-        });
-        allLineText.classed('inactive',function(d){
-            return ((dependsLinkAndText.indexOf(d.source.index) > -1) || (dependsLinkAndText.indexOf(d.target.index) > -1))
-        });
+        allNode.classed('inactive',(d)=>(dependsNode.indexOf(d.index) > -1));
+        allLink.classed('inactive',(d)=>((dependsLinkAndText.indexOf(d.source.index) > -1) || (dependsLinkAndText.indexOf(d.target.index) > -1)));
+        allLineText.classed('inactive',(d)=>((dependsLinkAndText.indexOf(d.source.index) > -1) || (dependsLinkAndText.indexOf(d.target.index) > -1)));
     } else {
         allNode.classed('inactive', false);
         allLink.classed('inactive', false);
         allLineText.classed('inactive', false);
     }
 }
-
 //填充数据和绑定节点的事件
 function update(json){
     //转换数据
     force.nodes(json.nodes);
     force.force("link").links(json.links);
-
+    //生成节点、连接线、连接线文字以及绑定悬浮框事件
     let link=_link(json,vis);
     let node=_node(json,vis);
     let linetext=_linetext(json,vis);
     let tp=_tp(highlightObject,tooltip);
-    tooltip.on('dblclick', function () {
+    //绑定悬浮窗事件
+    tooltip.on('dblclick',()=>{
             d3.event.stopPropagation();
         })
-        .on('mouseover', function () {
-            if (node.mouseoutTimeout) {
-                clearTimeout(node.mouseoutTimeout);
-                node.mouseoutTimeout = null;
-            }
+        .on('mouseover',()=>{
+            node.mouseoutTimeout&&clearTimeout(node.mouseoutTimeout);node.mouseoutTimeout = null;
         })
-        .on('mouseout', function () {
-            if (node.mouseoutTimeout) {
-                clearTimeout(node.mouseoutTimeout);
-                node.mouseoutTimeout = null;
-            }
-            node.mouseoutTimeout = setTimeout(function () {
-                tp.highlightToolTip(null);
-            }, 300);
+        .on('mouseout',()=>{
+            node.mouseoutTimeout&&clearTimeout(node.mouseoutTimeout);node.mouseoutTimeout = null;
+            node.mouseoutTimeout = setTimeout(()=>{tp.highlightToolTip(null);}, 300);
         });
-    node.on('contextmenu', function (d) {
-            if (node.mouseoutTimeout) {
-                clearTimeout(node.mouseoutTimeout);
-                node.mouseoutTimeout = null;
-            }
+    node.on('contextmenu',(d)=>{
+            node.mouseoutTimeout&&clearTimeout(node.mouseoutTimeout);node.mouseoutTimeout = null;
             tp.highlightToolTip(d);
             d3.event.preventDefault();
             d3.event.stopPropagation();
         })
-        .on('mouseover', function(d) {
-            if (node.mouseoutTimeout) {
-                clearTimeout(node.mouseoutTimeout);
-                node.mouseoutTimeout = null;
-            }
-        })
-        .on('mouseout', function () {
-            if (node.mouseoutTimeout) {
-                clearTimeout(node.mouseoutTimeout);
-                node.mouseoutTimeout = null;
-            }
-            node.mouseoutTimeout = setTimeout(function () {
-                tp.highlightToolTip(null);
-            }, 300);
+        .on('mouseover',(d)=>{node.mouseoutTimeout&&clearTimeout(node.mouseoutTimeout);node.mouseoutTimeout = null;})
+        .on('mouseout',()=>{
+            node.mouseoutTimeout&&clearTimeout(node.mouseoutTimeout);node.mouseoutTimeout = null;
+            node.mouseoutTimeout = setTimeout( ()=>{tp.highlightToolTip(null)}, 300);
         })
         .call(_nodeDrag(force,_tick,link,node,linetext));
-
-    //更新坐标函数
-    force.on("tick",function(){
-        _tick(link,linetext,node);
-    });
-
+    //重启模拟
     force.restart();
+    //更新坐标函数
+    force.on("tick",()=>{_tick(link,linetext,node)});
 }
 update(json);
 
-//双击页面
-d3.select("body").on('dblclick', function () {
+//双击页面还原隐藏的元素
+d3.select("body").on('dblclick',()=>{
     dependsNode = dependsLinkAndText = [];
     highlightObject(null);
     force.restart();
