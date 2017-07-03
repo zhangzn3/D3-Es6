c267af9035757e8b62f86909c2fc0d10aca03258
zhangzn3 <619025283@qq.com>
2017-07-03 20:19:29 +0800

    d3

--- src/pages/home/js/_link.js
+++ src/pages/home/js/_link.js
@@ -3,9 +3,9 @@ export default function(json,vis){
     _link=_link.data(json.links,(d)=>(`${d.source.name}_${d.target.name}`));
     _link.exit().remove();
     _link=_link.enter().append("svg:path").attr("class", "link").merge(_link)
-        .attr('stroke-width',(d)=>{
+        .attr('stroke-width',(d)=>{ //只有一个字段，暂时这样写
             let value=(d.value+"").match(/^\d+$/);
-            return !!value?value[0]*0.17:1;//只有一个字段，暂时这样写
+            return !!value?(value[0]*0.27<1?1:value[0]*0.27):1
         })
         .attr('id',(d)=>(d.source.index + '_' + d.target.index))
         //不应该有指向自己的关系 异常处理
